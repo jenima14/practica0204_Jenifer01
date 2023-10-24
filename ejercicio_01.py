39ccf332e7488214663436e235951e19eeeef5db
jenima14 <jmarticarr2@educacion.navarra.es>
2023-10-24 13:55:09 +0200

    ejercicio_01

---
+++ ejercicio_01.py
@@ -0,0 +1,7 @@
+variable = "contraseña"
+contraseña = input("Escribir una contaseña: ")
+if variable  == contraseña.lower():
+    print("La contraseña coincide")
+else:
+    print("La contraseña no coincide")
+    input()
