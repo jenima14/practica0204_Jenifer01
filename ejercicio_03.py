d5a28e38b0606d7ac736ca878818e49f4e724202
jenima14 <jmarticarr2@educacion.navarra.es>
2023-10-27 10:25:58 +0200

    ejercicio_03

---
+++ ejercicio_03.py
@@ -0,0 +1,6 @@
+numero_entero = int(input("Escribe un número entero: "))
+if numero_entero % 2 == 0:
+    print(numero_entero, "es un número par.")
+else:
+    print(numero_entero, "es un número impar.")
+input()
